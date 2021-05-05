59621cd193c82bda5557115672b2c43ebbd52b52
ISeeTheFuture <leesh4719@gmail.com>
2021-05-06 01:06:40 +0900

    졸려...

---
+++ src/brailleConverter.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstring>
+#include "string.h"
+
+class Braille {
+private:
+    // ㄱ ㄲ ㄴ ㄷ ㄸ ㄹ ㅁ ㅂ ㅃ ㅅ ㅆ ㅇ ㅈ ㅉ ㅊ ㅋ ㅌ ㅍ ㅎ
+    int[] top ={ 0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139, 0x3141, 0x3142, 0x3143, 0x3145, 0x3146, 0x3147, 0x3148, 0x3149, 0x314a, 0x314b, 0x314c, 0x314d, 0x314e };
+    // ㅏ ㅐ ㅑ ㅒ ㅓ ㅔ ㅕ ㅖ ㅗ ㅘ ㅙ ㅚ ㅛ ㅜ ㅝ ㅞ ㅟ ㅠ ㅡ ㅢ ㅣ
+    int[] mid = { 0x314f, 0x3150, 0x3151, 0x3152, 0x3153, 0x3154, 0x3155, 0x3156, 0x3157, 0x3158, 0x3159, 0x315a, 0x315b, 0x315c, 0x315d, 0x315e, 0x315f, 0x3160, 0x3161, 0x3162, 0x3163 };
+    // ㄱ ㄲ ㄳ ㄴ ㄵ ㄶ ㄷ ㄹ ㄺ ㄻ ㄼ ㄽ ㄾ ㄿ ㅀ ㅁ ㅂ ㅄ ㅅ ㅆ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ
+    int[] bot = { 0, 0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136, 0x3137, 0x3139, 0x313a, 0x313b, 0x313c, 0x313d, 0x313e, 0x313f, 0x3140, 0x3141, 0x3142, 0x3144, 0x3145, 0x3146, 0x3147, 0x3148, 0x314a, 0x314b, 0x314c, 0x314d, 0x314e };
+
+public:
+    int separater(string dest, string src) {
+        for(int i = 0; i < src.size(); i++) {
+            int topBuf = 0;
+            int midBuf = 0;
+            int botBuf = 0;
+            int ch = src.at(i);
+            if(ch >= 0xAC00 && ch <= 0xD7A3) {
+                botBuf = ch - 0xAC00;
+                topBuf = botBuf / (21 * 28);
+                dest += top[topBuf];
+                
+                botBuf = botBuf % (21 * 28);
+                midBuf = botBuf / 28;
+                dest += mid[midBuf];
+                
+                botBuf = botBuf % 28;
+                if(botBuf != 0) {
+                    dest += bot[botBuf];
+                }
+            }
+            return dest;
+        }
+    };
+
+    int strToBraille(string dest, string src) {
+
+    };
+}
